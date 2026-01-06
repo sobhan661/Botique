02bca864626aac456e21f9b54853544d2a9bfd36
sobhan661 <sobhantheimpaler@gmail.com>
2026-01-06 09:19:32 +0330

    feat: create CustomUserAdmin and register CustomUser in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,76 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    list_display = [
+        "get_full_name",
+        "phone_number",
+        "is_active",
+    ]
+
+    search_fields = ("phone_number", "first_name", "last_name", "email")
+    ordering = ("phone_number",)
+
+    fields = (
+        (
+            "Personal Information",
+            {
+                "fields": (
+                    "password",
+                    "phone_number",
+                    "first_name",
+                    "last_name",
+                    "email",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "last_login",
+                    "created_at",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "phone_number",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
