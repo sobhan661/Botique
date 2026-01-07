958d62199ef9080014a3962305974baecc2dfc58
sobhan661 <sobhantheimpaler@gmail.com>
2026-01-07 18:28:34 +0330

    feat: add item model to admin panel and add itemvariant model as an inline model to item model

--- items/admin.py
+++ items/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.humanize.templatetags.humanize import intcomma
 
-# Register your models here.
+from .models import ItemVariant, Item
+
+
+class ItemVariantInline(admin.TabularInline):
+    model = ItemVariant
+    extra = 0
+    min_num = 1
+    validate_min = True
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = [
+        "name",
+        "category",
+        "formatted_price",
+        "is_active",
+    ]
+    list_filter = [
+        "category",
+        "is_active",
+    ]
+    search_fields = ("name",)
+    inlines = [ItemVariantInline]
+
+    @admin.display(description="Price (Toman)")
+    def formatted_price(self, obj):
+        return intcomma(obj.price_toman)
