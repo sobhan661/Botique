df4aab60999bb0b6de972741e25c8d46bc25efcb
sobhan661 <sobhantheimpaler@gmail.com>
2026-01-08 12:10:46 +0330

    feat: add tests for usercreationform and userchangeform

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,6 +2,9 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
 class UserTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
@@ -19,6 +22,7 @@ class UserTests(TestCase):
         self.assertEqual(user.email, "testmail@email.com")
 
         self.assertTrue(user.is_active)
+        self.assertTrue(user.check_password("testpass123"))
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
@@ -38,5 +42,63 @@ class UserTests(TestCase):
         self.assertEqual(user.email, "testmail@email.com")
 
         self.assertTrue(user.is_active)
+        self.assertTrue(user.check_password("testpass123"))
         self.assertTrue(user.is_staff)
         self.assertTrue(user.is_superuser)
+
+
+class UserCreationFormTests(TestCase):
+    def test_user_creation_form_valid_data(self):
+        form = CustomUserCreationForm(
+            data={
+                "phone_number": "+989123456789",
+                "first_name": "testname",
+                "last_name": "testlastname",
+                "email": "testmail@email.com",
+                "password1": "testpass123",
+                "password2": "testpass123",
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+        user = form.save()
+
+        self.assertEqual(user.phone_number, "+989123456789")
+        self.assertEqual(user.first_name, "testname")
+        self.assertEqual(user.last_name, "testlastname")
+        self.assertEqual(user.email, "testmail@email.com")
+
+        self.assertTrue(user.check_password("testpass123"))
+
+
+class UserChangeFormTests(TestCase):
+    def setup(self):
+        self.User = get_user_model()
+        self.user = self.User.objects.create_user(
+            phone_number="+989123456789",
+            first_name="testname",
+            last_name="testlastname",
+            password="testpass123",
+            email="testmail@email.com",
+        )
+
+    def test_user_creation_form_valid_data(self):
+        form = CustomUserChangeForm(
+            data={
+                # "phone_number": "+989123456789",
+                "first_name": "testname",
+                "last_name": "testlastname",
+                "email": "testmail@email.com",
+                "is_active": True,
+            },
+        )
+
+        self.assertTrue(form.is_valid())
+        user = form.save()
+
+        # self.assertEqual(user.phone_number, "+989123456789")
+        self.assertEqual(user.first_name, "testname")
+        self.assertEqual(user.last_name, "testlastname")
+        self.assertEqual(user.email, "testmail@email.com")
+
+        # self.assertTrue(user.check_password("testpass123"))
