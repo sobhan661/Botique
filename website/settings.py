30aca359e86a3f0f8cd34dd4267449c133f837d1
sobhan661 <sobhantheimpaler@gmail.com>
2026-02-01 10:53:27 +0330

    update: fix bugs and compelete base.html

--- website/settings.py
+++ website/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = [BASE_DIR / "static"]
 
 # Using custom user model
 
